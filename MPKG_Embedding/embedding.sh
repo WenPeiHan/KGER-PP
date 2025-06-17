3e5e9ccd19b81dc881018d5acc78a2117f4afcbf
feng-xueqian <163419427+feng-xueqian@users.noreply.github.com>
2025-06-17 16:33:23 +0800

    Update embedding.sh

--- MPKG_Embedding/embedding.sh
+++ MPKG_Embedding/embedding.sh
@@ -1,8 +1,1 @@
-#python embedding.py --project_name output/TransE_768 --model TransE --hidden_dim 768 --max_steps 2000 --save_checkpoint_steps 500
-#python embedding.py --project_name output/DistMult_768 --model DistMult --hidden_dim 768 --max_steps 2000 --save_checkpoint_steps 500
-#python embedding.py --project_name output/ComplEx_768 --model ComplEx --hidden_dim 768 --max_steps 2000 --save_checkpoint_steps 500 --double_entity_embedding --double_relation_embedding
-#python embedding.py --project_name output/RotatE_768 --model RotatE --hidden_dim 768 --max_steps 2000 --save_checkpoint_steps 500 --double_entity_embedding
-#python embedding.py --project_name output/TransE_1024 --model TransE --hidden_dim 1024 --max_steps 2000 --save_checkpoint_steps 500
-#python embedding.py --project_name output/DistMult_1024 --model DistMult --hidden_dim 1024 --max_steps 2000 --save_checkpoint_steps 500
-#python embedding.py --project_name output/ComplEx_1024 --model ComplEx --hidden_dim 1024 --max_steps 2000 --save_checkpoint_steps 500 --double_entity_embedding --double_relation_embedding
-#python embedding.py --project_name output/RotatE_1024 --model RotatE --hidden_dim 1024 --max_steps 2000 --save_checkpoint_steps 500 --double_entity_embedding
+python embedding.py --project_name output/TransE_768 --model TransE --hidden_dim 768 --max_steps 2000 --save_checkpoint_steps 500
